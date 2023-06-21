c98831677f32082ebcf047d492fc0101375c0c75
ivanbondarenco <eivaanbondarenco@gmail.com>
2023-06-21 02:24:57 -0300

    Se agrega el proyecto

---
+++ parcialPoo2/src/parcialPoo2/Main.java
@@ -0,0 +1,19 @@
+package parcialPoo2;
+
+import java.io.IOException;
+
+public class Main {
+
+		public static void main(String[] args) throws IOException {
+			Archivo ar = new Archivo();
+			ar.leerArchivo("source/almacenamiento.in");
+
+			Celular c1 = new Celular(ar.getCantApp(), ar.getTamanioApp(), ar.getApps());
+			Almacenamiento al = new Almacenamiento (c1);
+			al.revisaryEliminarApps();
+			ar.escribirSalida(al);
+		}
+
+	}
+
+
